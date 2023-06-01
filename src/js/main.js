9113d140eba63423e533eed79de0ddb26205d99a
elsalvacion <elsalvacion2022@gmail.com>
2023-06-01 21:48:47 +0600

    about section completed

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+  });
+});
