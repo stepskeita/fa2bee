b00f1f78bb0c07a75d19c247a0c2b50a6c5a9c1a
elsalvacion <elsalvacion2022@gmail.com>
2023-06-12 00:01:36 +0600

    images compressed

--- src/js/fa2bee-laundry.js
+++ src/js/fa2bee-laundry.js
@@ -1,29 +1,27 @@
 const faqs = [
   {
     question: "HOW FAST CAN I GET MY ITEMS DELIVERED?",
-    answer: `<p class="text-black">You can have your items delivered within 2 days.</p>`,
+    answer: `You can have your items delivered within 2 days.`,
   },
   {
     question: "ARE ANY OF YOUR PRODUCTS A DANGER TO THE ENVIRONMENT?",
-    answer: `<p class="text-black">We use animal friendly products.</p>
-              <p>None of our products are harmful to the environment.</p>
-              `,
+    answer: `We use animal friendly products. None of our products are harmful to the environment.`,
   },
   {
     question: "WHAT DO I PUT MY DIRTY LAUNDRY ITEMS IN?",
-    answer: `<p class="text-black">For first time users, put it in a trash bag or any bag and when we return your clothes we will put it in a nice RIAHBEE bag, we kindly ask that you use those for the future orders.</p>`,
+    answer: `For first time users, put it in a trash bag or any bag and when we return your clothes we will put it in a nice RIAHBEE bag, we kindly ask that you use those for the future orders.`,
   },
   {
     question: "WHAT HAPPENS WHEN MY CLOTHING IS RUINED BY YOUR LAUNDRY MAT ?",
-    answer: `<p class="text-black">Please do call us immediately if this happens, such issues are rare but our customer service is what makes us different. we will do everything we can to quickly fix the problem.</p>`,
+    answer: `Please do call us immediately if this happens, such issues are rare but our customer service is what makes us different. we will do everything we can to quickly fix the problem.`,
   },
   {
     question: "FOR PICKUP AND DELIVERY , DO I HAVE TO BE HOME?",
-    answer: `<p class="text-black">When you make this request simply let us know where the laundry/ dry cleaning will be , most people put it in their front door. Whatever works for you ! the same thing goes for when we return your items.</p>`,
+    answer: `When you make this request simply let us know where the laundry/ dry cleaning will be , most people put it in their front door. Whatever works for you ! the same thing goes for when we return your items.`,
   },
   {
     question: "DO I BRING IN MY OWN DETERGENT?",
-    answer: `<p class="text-black">Unless you want to, we will provide the detergent for free.</p>`,
+    answer: `Unless you want to, we will provide the detergent for free.`,
   },
 ];
 
